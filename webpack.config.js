a2410f73f5feed27af1ed5e42630336a5039e06c
Wilfredo <casas.farach@gmail.com>
2020-04-15 23:11:42 +0200

    all worked but sass config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   module: {
@@ -23,12 +24,20 @@ module.exports = {
         test: /\.(png|svg|jpg|gif|jpeg)$/,
         use: ["file-loader"],
       },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
       filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
     }),
   ],
 };
